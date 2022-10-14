dbad495ed8aa02f91a01e1f1bcb4dd9a53472869
Jesus-Antonio-Villanueva-Hidalgo <jesusantonio.ja200@gmail.com>
2022-10-13 23:42:51 -0500

    BadeDatos

---
+++ Parcial2/BaseDeDatos/app.js
@@ -0,0 +1,21 @@
+let mysql = require('mysql')
+let fs = require('fs')
+let j2xls = require('json2xls')
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'world'
+  });
+   
+  connection.connect();
+   
+  connection.query('SELECT * FROM city', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+
+    var xls = j2xls(results);
+    fs.writeFileSync(`${__dirname}/Excel/data.xlsx`,xls,'binary')
+  });
+  connection.end();
