bcb9be38f3e1459f1f0436ae689a0a4ea66366e4
Jesus-Antonio-Villanueva-Hidalgo <jesusantonio.ja200@gmail.com>
2022-10-19 20:19:40 -0500

    crud

---
+++ Parcial2/CRUD/app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+let mysql = require('mysql2')
+const app = express()
+const port = 3002
+
+app.use(express.text())
+app.use(express.json())
+
+let con = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'world'
+})
+
+app.get('/city/:id',(req,res)=>{
+  const id = req.params.id;
+  con.query(`SELECT * FROM city WHERE ID =${id}`,function(error,results,fields){
+    res.send(results)
+    if(error) throw error
+  })
+});
+
+app.post('/city',(req,res)=>{
+  const {Name,CountryCode,District,Population} = req.body;
+  con.query(`INSERT INTO city (Name,CountryCode,District,Population) VALUES ('${Name}','${CountryCode}','${District}',${Population})`,function(error,results,fields){
+    res.json('Insertado correctamente')
+    res.status(results)
+    if(error) throw error
+  })
+});
+
+app.delete('/city/:id',(req,res)=>{
+  const id = req.params.id;
+  con.query(`DELETE FROM city WHERE ID =${id}`,function(error,results,fields){
+    res.json('Eliminado correctamente')
+    res.status(results)
+    if(error) throw error
+  })
+});
+
+app.listen(port,()=>{
+  console.log('La aplicacion esta en linea!')
+  console.log(__dirname)//donde estoy
+  console.log(__filename)//nombre del archivo
+});
+  
